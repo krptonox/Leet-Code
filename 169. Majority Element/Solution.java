3b6d939f8eac267640c3c297710823016d7a0b2b
krptonox <aryanyadav182007@gmail.com>
2025-10-15 08:33:15 +0530

    Updated a deleted Folder

--- 169. Majority Element/Solution.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int x = nums[nums.length/2];
-
-        System.out.println(x);
-        return nums[nums.length/2];
-    }
-}
--- 69. Sqrt(x)/Solution.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int mySqrt(int x) {
-        int ans = 0;
-        ans = (int)Math.sqrt(x);
-        return ans;
-    }
-}
