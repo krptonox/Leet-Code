d6bd36718f607e085d33d0aa56c05bc39deeb02f
Aryan <aryanyadav182007@gmail.com>
2026-01-21 14:20:16 +0530

    Time: 2 ms (18.22%) | Memory: 43.4 MB (83.06%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -3,12 +3,11 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]>nums[j]){
-                    int temp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = temp;
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
                 }
             }
         }
-        return;
     }
 }
