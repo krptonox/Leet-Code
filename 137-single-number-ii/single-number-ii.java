01445113e8f274ef54311ba015155be96370be9f
Aryan <aryanyadav182007@gmail.com>
2026-01-28 15:23:25 +0530

    Time: 107 ms (5.02%) | Memory: 46.7 MB (8.22%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
+       // Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
             int count = 0;
             for(int j=0;j<nums.length;j++){
