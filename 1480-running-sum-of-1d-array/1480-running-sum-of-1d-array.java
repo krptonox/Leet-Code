4b897d75c7e97289d97a4fcd0ca70fb43d94dd87
Aryan <aryanyadav182007@gmail.com>
2026-03-30 02:27:36 +0530

    Time: 0 ms (100%), Space: 44.5 MB (28.26%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -7,15 +7,6 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             prefix[i] = prefix[i-1]+nums[i];
         }
-
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                currsum = i==0 ? prefix[j]: prefix[j]-prefix[i-1];
-                if(currsum>maxsum){
-                    maxsum=currsum;
-                }
-            }
-        }
         return prefix;
     }
 }
