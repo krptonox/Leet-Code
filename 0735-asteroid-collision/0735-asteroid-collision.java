62087860ed262f5df6fcf95ccce42dff51c7fe3c
Aryan <aryanyadav182007@gmail.com>
2026-04-24 11:29:21 +0530

    Time: 5 ms (65.71%), Space: 47.5 MB (7.75%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -2,37 +2,36 @@ class Solution {
     public int[] asteroidCollision(int[] as) {
         Stack<Integer> st = new Stack<>();
         for(int i=0;i<as.length;i++){
-            boolean destroyed = false;
             int ast = as[i];
-           if(st.isEmpty() || ast > 0 || st.peek() < 0) {
+            Boolean destroy = false;
+            if(st.isEmpty() || ast>0 || st.peek()<0 ){
                 st.push(ast);
             }
-            else if(!st.isEmpty() && st.peek()>0){
-                while(!st.isEmpty()&& st.peek()>0 && ast<0){
-                    int Abst = Math.abs(ast);
-                    int chk = st.pop();
-                    if(Abst>chk){
-                        continue;
-                    }
-                    else if(Abst<chk){
-                        st.push(chk);
-                        destroyed = true;
-                        break;
-                    }
-                    else{
-                        destroyed = true;
-                        break;
-                    }
+            else if(!st.isEmpty() && ast < 0 ){
+            while(!st.isEmpty() && ast<0 && st.peek()>0){
+                int Abst = Math.abs(ast);
+                int chk = st.pop();
+                if(Abst>chk){
+                    continue;
                 }
-                if(!destroyed){
-                    st.push(ast);
+                else if(Abst<chk){
+                    destroy = true;
+                    st.push(chk);
+                    break;
+                }
+                else{
+                    destroy = true;
+                    break;
                 }
             }
-            
+            if(!destroy){
+                st.push(ast);
+            }
+            }
         }
-
+        
         int s = st.size();
-        int[] arr = new int[s];
+        int arr[] = new int[s];
         for(int i=s-1;i>=0;i--){
             arr[i]=st.pop();
         }
