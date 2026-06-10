cb489a80a483cb741011b66eb5014f12663b58ae
Aryan <aryanyadav182007@gmail.com>
2026-06-11 00:01:33 +0530

    Time: 4 ms (97.07%), Space: 45 MB (51.13%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            } 
+            else if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } 
+            else {
+                stack.push(part);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "/";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String dir : stack) {
+            sb.append("/").append(dir);
+        }
+
+        return sb.toString();
+    }
+}
