15a6e43b2aa813dafb575db2481e46118a3b6dd0
Aryan <aryanyadav182007@gmail.com>
2026-08-21 11:16:24 +0530

    Time: 9 ms (7.55%), Space: 44.5 MB (7.94%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bull = 0;
+        int cow = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        boolean[] Bull = new boolean[secret.length()];
+
+        int len = guess.length();
+        for(int i=0;i<len;i++){
+            if(secret.charAt(i) == guess.charAt(i)){
+                bull++;
+                Bull[i] = true;
+            }
+        }
+
+        for(int i=0;i<len;i++){
+            if(Bull[i]) continue;
+            map.put(secret.charAt(i),map.getOrDefault(secret.charAt(i),0)+1);
+        }
+
+        for(int i=0;i<len;i++){
+            if(Bull[i]) continue;
+            char ch = guess.charAt(i);
+            
+            if(map.getOrDefault(ch,0) > 0){
+                cow++;
+                map.put(ch, map.get(ch) - 1);
+            }
+        }
+
+        System.out.print(bull);
+        System.out.print(cow);
+        String str = Integer.toString(bull);
+        String str1 = Integer.toString(cow);
+        
+        return bull+"A"+cow+"B";
+    }
+}
