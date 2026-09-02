f0bc2c51991f829315581b210b09d8b1bc589960
Aryan <aryanyadav182007@gmail.com>
2026-09-02 10:48:10 +0530

    Time: 0 ms (100%), Space: 42.4 MB (12.85%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] ar = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0) ar[i][j] = 1;
+                else ar[i][j] = ar[i][j-1] + ar[i-1][j]; 
+            }
+        }
+        return ar[m-1][n-1];
+    }
+}
